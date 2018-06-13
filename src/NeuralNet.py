bd1ffc8976384bcd28769550606684c54b1d841c
rfishy1 <richard.carl.fisher@gmail.com>
2018-06-13 22:15:21 +0200

    Add approximate gradient descent

--- src/NeuralNet.py
+++ src/NeuralNet.py
@@ -30,6 +30,33 @@ class Network:
         cost = 1/(2*inputs.shape[1])*np.sum(np.linalg.norm(outputs-a,axis=0),axis=0)
         return cost
     
+    def train(self, inputs, outputs, num_epochs):
+        """Train the network for num_epochs"""
+        
+        step_size = 0.00000001
+        learning_rate = 1.0
+        
+        for k in range(num_epochs):
+            current_cost = self.cost(inputs, outputs)
+            for layer in self.layers:
+                for node in layer.nodes:
+                    for w_ind in range(len(node.w)):
+                        # Perturb this by step_size
+                        node.w[w_ind] += step_size
+                        partial_cost = (self.cost(inputs, outputs) - current_cost)/step_size
+                        node.w[w_ind] -= step_size
+                        node.w_to_update[w_ind] = node.w[w_ind] - learning_rate*partial_cost
+                    
+                    node.b +=step_size
+                    partial_cost = (self.cost(inputs, outputs) - current_cost)/step_size
+                    node.b -= step_size
+                    node.b_to_update = node.b - learning_rate*partial_cost
+            for layer in self.layers:
+                layer.update_parameters()
+                        
+        return self.cost(inputs, outputs)
+        
+    
 class Layer:
     def __init__(self, num_nodes, num_inputs):
         """Initialises a Layer object"""
@@ -41,16 +68,24 @@ class Layer:
         a = np.concatenate([x.sim(inputs) for x in self.nodes], axis=0)
         return a
     
+    def update_parameters(self):
+        for node in self.nodes:
+            node.update_parameters()
+            
+    
 class Neuron:
     """A neural network layer comprises a number of neurons"""
     
     def __init__(self, num_inputs):
         self.w = np.random.normal(size=[1,num_inputs])
         self.b = np.random.normal(size=[1,1])
+        self.w_to_update = np.zeros_like(self.w)
+        self.b_to_update = np.zeros_like(self.b)
         
     def sim(self, inputs):
         a = ut.sigmoid(np.dot(self.w,inputs)+self.b)
         return a
-     
-
-        
+    
+    def update_parameters(self):
+        self.w = np.array(self.w_to_update)
+        self.b = np.array(self.b_to_update)
--- src/test.py
+++ src/test.py
@@ -24,6 +24,25 @@ np.random.seed(6)
 net = nn.Network([1,20,2])
 
 inputs = np.array([np.arange(-10.0,10.0,0.1)])
-outputs = np.concatenate((np.sin(inputs),np.cos(inputs)))
+outputs = np.concatenate((0.5*np.sin(inputs)+0.5,0.5*np.cos(inputs)+0.5))
 
 print(net.cost(inputs, outputs))
+
+plt.plot(np.transpose(inputs), np.transpose(outputs))
+plt.plot(np.transpose(inputs), np.transpose(net.sim(inputs)))
+
+
+#%% Try training the network
+np.random.seed(7)
+net = nn.Network([1,10,1])
+
+inputs = np.array([np.arange(-3.0,3.0,0.2)])
+outputs =0.5*np.sin(inputs)+0.5
+
+for k in range(10):
+    cost = net.train(inputs, outputs, 1000)
+    plt.plot(np.transpose(inputs), np.transpose(outputs))
+    plt.plot(np.transpose(inputs), np.transpose(net.sim(inputs)))
+    plt.pause(0.05)
+    
+    print(cost)
